11e47dcbfe4c8b642a13fe2e8b68b671ddcd0d1b
yuthikaarakesh123 <86779906+yuthikaarakesh123@users.noreply.github.com>
2021-07-01 16:53:00 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+var box; 
+function setup() {
+
+  createCanvas(400,400);
+  box=createSprite(200,200,30,30)
+}
+
+function draw() 
+{
+  background(30);
+  if (keyIsDown (RIGHT_ARROW)) {
+box.position.x=box.position.x+5
+    
+  }
+  if (keyIsDown (LEFT_ARROW)) {
+    box.position.x=box.position.x-5
+        
+      }
+drawSprites();
+}
+
+
+
+
